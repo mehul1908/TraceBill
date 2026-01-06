57f3ef6fddadbb1eaa976321965d444d755f0c46
mehul1908 <135810448+mehul1908@users.noreply.github.com>
2026-01-06 09:24:37 +0530

    intial commit

---
+++ src/test/java/com/tracebill/TraceBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracebill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
